257a8e84ae24e56f7d80ed94f1bea5fce6921d30
mzucl <146825811+mzucl@users.noreply.github.com>
2024-09-29 19:32:01 +0100

    Add files via upload

---
+++ codes/exportToCSV.m
@@ -0,0 +1,44 @@
+function [] = exportToCSV(ids, data, patientsECG, header)
+%exportToCSV Export data to csv files
+    % There is a separate csv file for each mode and each band
+
+% Get the field names of the struct
+modes = fieldnames(data);
+
+% Export ECG data
+ecgMatrix = cell2mat(patientsECG);
+% Add column names
+ecgTable  = array2table(ecgMatrix, 'VariableNames', modes);
+
+% Add ID column
+ecgTable  = addvars(ecgTable, ids, 'Before', 1, 'NewVariableNames', 'ID');
+
+% Export the table to a CSV file
+writetable(ecgTable, 'ecg.csv');
+
+for i = 1:numel(modes)
+    mode = modes{i};
+    resMode = data.(mode);
+
+    bands = fieldnames(resMode);
+
+    for j = 1:numel(bands)
+        band = bands{j};
+        res  = resMode.(band);
+        
+        resMatrix = cell2mat(res);
+        % Add column names
+        resTable  = array2table(resMatrix, 'VariableNames', header);
+
+        % Add ID column
+        resTable  = addvars(resTable, ids, 'Before', 1, 'NewVariableNames', 'ID');
+
+        csvFileName = [mode '_' band '.csv'];
+        
+        % Export the table to a CSV file
+        writetable(resTable, csvFileName);
+    end
+end
+
+
+end
---
+++ codes/processECG.m
@@ -0,0 +1,48 @@
+function [hrRes] = processECG(data, sr)
+%processECG Extract the HR (heart rate) for different modes
+    % 1s overlap
+    overlap = 500;
+
+    modes = fieldnames(data);
+
+    hrRes = zeros(numel(modes), 1);
+
+    for i = 1:numel(modes)
+        ecgSignal = [];
+
+        mode     = modes{i};
+        modeData = data.(mode);
+        
+        numOfTrials = size(modeData, 2);
+
+        for j =1 :numOfTrials
+            ecgSignal = [ecgSignal modeData(1:overlap, j)'];
+
+            if j == numOfTrials
+                ecgSignal = [ecgSignal modeData(overlap+1:end, j)']; 
+            end
+        end
+   
+        % Define the sampling frequency (Hz)
+        fs = sr; % Replace with the actual sampling frequency of your ECG signal
+        
+        % Preprocess the ECG signal: Apply a bandpass filter to remove noise
+        % Define filter parameters
+        lowCutoff = 0.5; % Low cutoff frequency in Hz
+        highCutoff = 50; % High cutoff frequency in Hz
+        [b, a] = butter(2, [lowCutoff, highCutoff] / (fs / 2), 'bandpass');
+        filteredECG = filtfilt(b, a, ecgSignal);
+        
+        % Detect R-peaks
+        [~, rPeaks] = findpeaks(filteredECG, 'MinPeakHeight', 0.5, 'MinPeakDistance', fs * 0.6);
+        
+        % Calculate R-R intervals (differences between successive R-peaks)
+        rrIntervals = diff(rPeaks) / fs; % R-R intervals in seconds
+        
+        % Convert R-R intervals to heart rate (beats per minute)
+        heartRate = 60 ./ rrIntervals; % Heart rate in bpm
+        
+       
+        hrRes(i) = mean(heartRate);
+    end
+end
