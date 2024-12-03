060094fa6506df88bf75b7f9a207f1d90873f389
mzucl <146825811+mzucl@users.noreply.github.com>
2024-12-03 09:40:39 +0100

    Delete codes directory
    Upload a new version of the code

--- codes/getPatientFileNameAndId.m
+++
@@ -1,9 +0,0 @@
-function [fileName, patientId] = getPatientFileNameAndId(path)
-%getPatientFileNameAndId 
-%   Extract file name and patient id from file path
-
-parts = split(path, '.');
-
-fileName  = char(parts(1));
-patientId = fileName(end-3:end);
-end
