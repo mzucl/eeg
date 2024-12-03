43763c50686f91b364f6e8a4c048b09433fb0dd9
mzucl <146825811+mzucl@users.noreply.github.com>
2024-12-03 09:44:13 +0100

    Update getPatientFileNameAndId.m

--- codes/getPatientFileNameAndId.m
+++ codes/getPatientFileNameAndId.m
@@ -1,9 +1,9 @@
 function [fileName, patientId] = getPatientFileNameAndId(path)
-%getPatientFileNameAndId 
-%   Extract file name and patient id from file path
-
-parts = split(path, '.');
-
-fileName  = char(parts(1));
-patientId = fileName(end-3:end);
+  % getPatientFileNameAndId 
+  % Extract file name and patient id from file path
+  
+  parts = split(path, '.');
+  
+  fileName  = char(parts(1));
+  patientId = fileName(end-3:end);
 end
